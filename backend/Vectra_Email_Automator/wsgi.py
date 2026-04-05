6748053edc7bb85399f230499b4b1bfa130f0b47
Areeb Khan <areebkhan1005@gmail.com>
2026-04-05 15:20:33 +0530

    Initialised Django files

---
+++ backend/Vectra_Email_Automator/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Vectra_Email_Automator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vectra_Email_Automator.settings')
+
+application = get_wsgi_application()
